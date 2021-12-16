2074880763464b519b53d6f204140895ff49f0b0
Artin <alex_91_100@live.se>
2021-12-16 15:17:42 +0100

    Add project files.

---
+++ Errors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Errors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<UserError> UserErrors = new List<UserError>();
+        }
+    }
+}
---
+++ Errors/UserError.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errors
+{
+    public abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+    public class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return ("You tried to use a numeric inputin a text only field. This fired an error!");
+        }
+    }
+
+    public class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return ("You tried to use a text input in a numericonly field. This fired an error!");
+        }
+}
+
+}
