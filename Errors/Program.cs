589aa5adbfcac2580b4eb20e4b3a6a67f0557f19
Artin <alex_91_100@live.se>
2021-12-17 10:02:42 +0100

    Fully finished with 3.4. :)

--- Errors/Program.cs
+++ Errors/Program.cs
@@ -10,6 +10,28 @@ namespace Errors
             Console.WriteLine("Hello World!");
 
             List<UserError> UserErrors = new List<UserError>();
+            UserErrors.Add(new NumericInputError());
+            UserErrors.Add(new NumericInputError());
+            UserErrors.Add(new TextInputError());
+
+            foreach (var error in UserErrors)
+            {
+                Console.WriteLine(error.UEMessage());
+            }
+
+            Console.WriteLine();
+
+            UserErrors.Add(new ArrayElementError());
+            UserErrors.Add(new ObjectArgumentError());
+            UserErrors.Add(new MethodCallError());
+            UserErrors.Add(new ArrayElementError());
+            UserErrors.Add(new MethodCallError());
+
+            // we will try to go through the UserErrors list again, this time with extra instances
+            foreach (var error in UserErrors)
+            {
+                Console.WriteLine(error.UEMessage());
+            }
         }
     }
 }
--- Errors/UserError.cs
+++ Errors/UserError.cs
@@ -15,7 +15,7 @@ namespace Errors
     {
         public override string UEMessage()
         {
-            return ("You tried to use a numeric inputin a text only field. This fired an error!");
+            return ("You tried to use a numeric input in a text only field. This fired an error!");
         }
     }
 
@@ -25,6 +25,29 @@ namespace Errors
         {
             return ("You tried to use a text input in a numericonly field. This fired an error!");
         }
-}
+    }
 
+    public class ArrayElementError : UserError
+    {
+        public override string UEMessage()
+        {
+            return ("You tried to place a variable that is not matching the data type of the string. This fired an error!");
+        }
+    }
+
+    public class ObjectArgumentError : UserError
+    {
+        public override string UEMessage()
+        {
+            return ("You used the wrong data type for one or more arguments when creating an object. This fired an error!");
+        }
+    }
+
+    public class MethodCallError : UserError
+    {
+        public override string UEMessage()
+        {
+            return ("You did not match the arguments with the method parameters when calling a method. This fired an error!");
+        }
+    }
 }
